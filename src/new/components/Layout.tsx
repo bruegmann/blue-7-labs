648b4463d601126ed1eda66cb3b039d09030b461
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2020-06-15 13:33:29 +0200

    Menu toggle button

--- src/new/components/Layout.tsx
+++ src/new/components/Layout.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import List from "../../icons/List";
 
 export interface ILayoutProps {
     children: ReactNode;
@@ -9,9 +10,14 @@ export interface ILayoutProps {
 export function Layout({ children, appLogo, appTitle }: ILayoutProps) {
     return (
         <>
-            <header className="bg-theme p-2 d-flex align-items-center" style={{ height: "48px" }}>
-                <img src={appLogo} alt={appTitle} className="h-100" />
-                <h4 className="text-white font-weight-light ml-2 mb-0">{appTitle}</h4>
+            <header className="bg-theme d-flex align-items-center" style={{ height: "48px" }}>
+                <button className="btn btn-trans btn-lg p-0 d-flex align-items-center justify-content-center rounded-0 mr-2 h-100" style={{ width: "48px" }}>
+                    <List />
+                </button>
+                <div className="p-2 d-flex h-100">
+                    <img src={appLogo} alt={appTitle} className="h-100" />
+                    <h4 className="text-white font-weight-light ml-2 mb-0">{appTitle}</h4>
+                </div>
             </header>
 
             {children}
